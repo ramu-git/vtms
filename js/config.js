cb57b7a4110a3a6d39bae5640e202fc8e215b173
Ramu-git <ramu.cloud@outlook.com>
2021-09-09 13:30:13 -0400

    Baseline1

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_B5cdOpkXu',
+        userPoolClientId: '6metfvl9b2cb4qtrm9gcb0k3b8',
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: 'https://kp55c6hss6.execute-api.us-east-1.amazonaws.com/prod'
+    }
+};
